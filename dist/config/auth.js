dec9652e7a711a19b147bf967105a7f7274c8283
Fernando Corrêa da Silva <fernando_silvasc@hotmail.com>
2021-01-09 21:00:16 -0800

    Add heroku configuration

---
+++ dist/config/auth.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    jwt: {
+        secret: '05e9996a20e94f5fc415f396c5b8a6f5',
+        expiresIn: '1d',
+    },
+};
---
+++ ormconfig.js
@@ -0,0 +1,10 @@
+console.log(process.env.ENTITIES_PATH)
+module.exports = {
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: [`${process.env.ENTITIES_PATH}`],
+  migrations: [`${process.env.MIGRATIONS_PATH}`],
+  cli: {
+    migrationsDir: './src/database/migrations',
+  },
+};
